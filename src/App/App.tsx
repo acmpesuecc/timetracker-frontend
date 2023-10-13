60c079cbbed4528705a02d1ab8c68c0317d2e077
Akshat Navlani <akshatnavlani1@gmail.com>
2023-10-14 02:17:00 +0530

    issue 3

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -16,7 +16,7 @@ import {Sheet} from "../gql/graphql";
 import React, {useState} from "react";
 import {Add} from "@mui/icons-material";
 import AddSheetModal from "./AddSheetModal";
-
+<link rel="stylesheet" href="button.css" />
 export default function App() {
     const sheets = useSheets()
     const navigate = useNavigate()
@@ -40,7 +40,8 @@ export default function App() {
                         return <Grid key={sh.id}>
                             <Card onClick={oc} elevation={3} sx={{height: "100%"}}>
                                 <Grid container p={2} height={"100%"} alignItems={"center"}>
-                                    <Typography>{sh.name}</Typography>
+                                    
+                                    <Button id="bt" type={"submit"} variant={"text"} sx={{alignSelf: "stretch"}} >{sh.name}</Button>
                                 </Grid>
                             </Card>
                         </Grid>
